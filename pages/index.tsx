13ef1d3f2379ece0e4c1360179780791a1a10462
karmasakshi <karmasakshi@users.noreply.github.com>
2021-01-11 01:22:33 +0400

    feat: add page component to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
+import Page from '../components/page/page';
+
 export interface HomePageProps { }
 
 const HomePage: (props: HomePageProps) => JSX.Element = (): JSX.Element => (
 
-  <div>Welcome to Next.js!</div>
+  <Page title="">
+    <div>Welcome to Next.js!</div>
+  </Page>
 
 );
 
