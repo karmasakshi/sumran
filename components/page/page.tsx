923bd6c15c0a6315e76de86665ae8ba3526a62ed
karmasakshi <karmasakshi@users.noreply.github.com>
2021-01-11 16:06:01 +0400

    feat: update styles

--- components/page/page.tsx
+++ components/page/page.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import { ReactNode } from 'react';
-import styles from './page.module.scss';
 
 interface PageProps {
   children: ReactNode;
@@ -9,13 +8,13 @@ interface PageProps {
 
 const Page: (props: PageProps) => JSX.Element = ({ children, title }: PageProps): JSX.Element => (
 
-  <div className={styles.page}>
+  <>
 
     <Head><title>{'Sumran Organic Farms' + (title.length ? ' - ' + title : '')}</title></Head>
 
-    <section>{children}</section>
+    <section className="pb-32 pt-16 px-4 text-center">{children}</section>
 
-  </div>
+  </>
 
 );
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import Page from '../components/page/page';
-import styles from '../styles/home-page.module.scss';
 
 export interface HomePageProps { }
 
@@ -8,10 +7,12 @@ const HomePage: (props: HomePageProps) => JSX.Element = (): JSX.Element => (
 
   <Page title="">
 
-    <div className={styles.heroContainer}>
-
+    <div>
       <Image src="/logo.png" alt="Logo" height={360} width={360} />
+    </div>
 
+    <div>
+      <h2>Fresh veggies, harvested weekly.</h2>
     </div>
 
   </Page>
