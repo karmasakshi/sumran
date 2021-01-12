645dd5e72f698cf26fe9bbd220835f0d871e0ce7
karmasakshi <karmasakshi@users.noreply.github.com>
2021-01-12 18:40:46 +0400

    feat: update csp header

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const headers = async () => [
       },
       {
         key: 'Content-Security-Policy',
-        value: process.env.NEXT_PUBLIC_IS_PRODUCTION === 'true' ? "upgrade-insecure-requests; base-uri 'self'; default-src 'self'; connect-src 'self'; font-src 'self' https://fonts.gstatic.com; form-action 'none'; frame-ancestors 'none'; img-src 'self' data:; object-src 'none'; style-src 'self' https://fonts.googleapis.com 'unsafe-inline'" : ''
+        value: process.env.NEXT_PUBLIC_IS_PRODUCTION === 'true' ? "upgrade-insecure-requests; base-uri 'self'; default-src 'none'; connect-src 'self'; font-src 'self' https://fonts.gstatic.com; form-action 'none'; frame-ancestors 'none'; img-src 'self' data:; manifest-src 'self'; object-src 'none'; script-src-elem 'self'; style-src 'self' https://fonts.googleapis.com 'unsafe-inline'; require-trusted-types-for 'script'" : ''
       },
       {
         key: 'Permissions-Policy',
