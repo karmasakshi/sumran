a95e9cae02431c6817316082202721f9b174ef18
karmasakshi <karmasakshi@users.noreply.github.com>
2021-01-14 12:02:32 +0400

    fix: csp header

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const headers = async () => [
       },
       {
         key: 'Content-Security-Policy',
-        value: process.env.NEXT_PUBLIC_IS_PRODUCTION === 'true' ? "upgrade-insecure-requests; default-src 'none'; connect-src https://hive.splitbee.io/i; font-src https://fonts.gstatic.com; img-src 'self' data:; manifest-src 'self'; script-src 'self' https://cdn.splitbee.io/sb.js 'sha256-yjbYreQDRCum38k0q7V/TkgD+KjqDDWwLI7T6xX31dQ='; style-src 'self' https://fonts.googleapis.com 'unsafe-inline'; require-trusted-types-for 'script'" : ''
+        value: process.env.NEXT_PUBLIC_IS_PRODUCTION === 'true' ? "base-uri 'none'; connect-src https://hive.splitbee.io/i; default-src 'none'; font-src https://fonts.gstatic.com; img-src 'self' data:; manifest-src 'self'; script-src 'self' https://cdn.splitbee.io/sb.js 'sha256-yjbYreQDRCum38k0q7V/TkgD+KjqDDWwLI7T6xX31dQ='; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; require-trusted-types-for 'script'; upgrade-insecure-requests;" : ''
       },
       {
         key: 'Permissions-Policy',
